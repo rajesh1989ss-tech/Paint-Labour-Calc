d78da8fad95c77b0e3dde1675077ffd3fe149bbc
rajesh1989ss-tech <rajesh1989ss@gmail.com>
2026-07-19 22:15:23 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "paintlog-v7";
+const CACHE = "paintlog-v8";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 
 self.addEventListener("install", e => {
